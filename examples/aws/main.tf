84d1cf8fb54d685789cc00093c058b64a214f450
Richard Mah <dick.mah@gmail.com>
2020-06-05 12:27:41 -0400

    add helm provider

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -19,7 +19,15 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "1.10"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
 }
 
 module "network" {
